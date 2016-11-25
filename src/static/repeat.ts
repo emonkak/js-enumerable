02fe3e26bf5b1d763ffa6f32060d191d1419a1a2
Shota Nozaki <emonkak@gmail.com>
2016-11-25 16:42:50 +0900

    Refactor repeat()

--- src/static/repeat.ts
+++ src/static/repeat.ts
@@ -2,13 +2,17 @@ import EmptyPartition from '../internal/EmptyPartition';
 import { argumentOutOfRange } from '../internal/errors';
 import RepeatPartition from '../internal/RepeatPartition';
 
-export default function repeat<TSource>(element: TSource, count: number = Infinity): Iterable<TSource> {
+export default function repeat<TSource>(element: TSource, count?: number): Iterable<TSource> {
     if (count < 0) {
         throw argumentOutOfRange('count');
     }
 
     if (count == 0) {
         return new EmptyPartition<TSource>();
+    }
+
+    if (count == null) {
+        count = Infinity;
     }
 
     return new RepeatPartition(element, count);
